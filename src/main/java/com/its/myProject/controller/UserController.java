d0c1c674f2f5e3ec13c2100c60ce044af21ce176
NOMANSARDAR <nsardar@studenti.laziodigital.it>
2024-05-28 15:30:24 +0200

    added controller class and a path on it

--- src/main/java/com/its/myProject/controller/UserController.java
+++ src/main/java/com/its/myProject/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.its.myProject.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam (value = "world", defaultValue = "none") String name){
+         return "Hello " + name;
+    }
 }
